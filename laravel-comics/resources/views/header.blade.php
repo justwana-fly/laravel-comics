c4a9558a8eff18cef303560d49bb1cacd68f009c
Nicolae <nicolaibarbarasa@gmail.com>
2024-05-20 21:27:34 +0200

    add some css and jumbotrone

--- laravel-comics/resources/views/header.blade.php
+++ laravel-comics/resources/views/header.blade.php
@@ -1,5 +1,19 @@
 <!-- header.blade.php -->
 
-<header class="bg-dark text-white text-center py-4">
-    <h1>DC Comics Collection</h1>
+<header >
+
+    <ul class="nav justify-content-center py-3 bg-light p-4">
+        <a href=""><li>CHARACTER</li></a>
+        <a href=""><li>COMICS</li></a>
+        <a href=""><li>MOVIE</li></a>
+        <a href=""><li>TV</li></a>
+        <a href=""><li>GAMES</li></a>
+        <a href=""><li>COLLECTIBLES</li></a>
+        <a href=""><li>VIDEOS</li></a>
+        <a href=""><li>FANS</li></a>
+        <a href=""><li>NEWS</li></a>
+        <a href=""><li>SHOP</li></a>
+    </ul>
+
 </header>
+
